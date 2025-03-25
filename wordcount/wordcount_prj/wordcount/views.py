a948cdc0ebe73c0437d875b107d1ff8abee22324
zyecastle <nesong722@gmail.com>
2025-03-25 23:59:09 +0900

    [0326]조예성-3차세션 과제

---
+++ wordcount/wordcount_prj/wordcount/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def word_count(request):
+    return render(request, 'word_count.html')
+
+def result(request):
+    entered_text = request.GET['fulltext']
+    word_list = entered_text.split()
+
+    word_dictionary = {}
+
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+
+    total_word_count = len(word_list)
+    total_char_count = len(entered_text)
+    char_no_space = len(entered_text.replace(" ",""))
+
+    max_freq = max(word_dictionary.values())
+    most_frequent_words = [word for word, freq in word_dictionary.items() if freq == max_freq]
+    return render(request, 'result.html', {'alltext':entered_text, 'dictionary':word_dictionary.items(), 'total' : total_word_count,
+    'total_chars': total_char_count, 'no_space': char_no_space, 'most_words': most_frequent_words, 'max_freq': max_freq,})
+
+def hello(request):
+    name = request.GET.get('name')
+    return render(request, 'hello.html', {'username': name})
